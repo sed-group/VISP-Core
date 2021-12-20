85430e325dc4b42e5362c8eb2be2b29498996a02
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2021-12-20 10:21:03 +0100

    paperc updates

---
+++ data/automotive_architecture.md
@@ -0,0 +1,15 @@
+Car
+    Vehicle frame / Chassis
+    Controls
+        Steering
+        Go
+        Brake
+        ...
+    Suspension
+        Front
+        Rear
+    Powertrain
+        Engine / Motor: Type, Size, Location...
+        Drivetrain
+    Seating
+    Safety
--- visp/model.py
+++ visp/model.py
@@ -1,66 +1,99 @@
-#!/usr/bin/env python
-import uuid
+"""
+Definition of models using Pydantic.
+"""
+
+import json
+from typing import List, Optional
+from pydantic import BaseModel, UUID4
 
 
-class Model():
+class Parameter(BaseModel):
+    def __init__(
+        self,
+        name: str,
+        description: Optional[str] = None,
+        parameter_type: Optional[str] = None,
+        min: Optional[float] = None,
+        max: Optional[float] = None,
+        value: Optional[float] = None,
+    ):
+        self.uuid: UUID4
+        self.name = name
+        self.description = description
+        self.parameter_type = (parameter_type,)
+        self.min = (min,)
+        self.max = (max,)
+        self.value = value
 
-    def __init__(self):
-        pass
+    def __repr__(self):
+        return "<Parameter: name=%s, description=%s>" % (self.name, self.description)
 
 
-class Stakeholder:
-    def __init__(self,
-                 name: str,
-                 description: str = None,
-                 role: str = None,
-                 organization: str = None):
-        self.uuid = uuid.uuid4()
+class Stakeholder(BaseModel):
+    def __init__(
+        self,
+        name: str,
+        description: Optional[str] = None,
+        role: Optional[str] = None,
+        organization: Optional[str] = None,
+    ):
+        self.uuid: UUID4
         self.name = name
         self.description = description
         self.role = role
         self.organization = organization
 
     def __repr__(self):
-        return "<Stakeholder: name=%s, description=%s, role=%s, organization=%s>" % (self.name, self.description, self.role, self.organization)
+        """docstring"""
+        return "<Stakeholder: name=%s, description=%s, role=%s, organization=%s>" % (
+            self.name,
+            self.description,
+            self.role,
+            self.organization,
+        )
 
 
-class Expectation:
-    def __init__(self,
-                 name,
-                 description=None,
-                 stakeholder=None,
-                 category=None):
-        self.uuid = uuid.uuid4()
+class Expectation(BaseModel):
+    def __init__(
+        self,
+        name: str,
+        description: Optional[str] = None,
+        stakeholder: Optional[str] = None,
+        category: Optional[str] = None,
+        parameter: Optional[Parameter] = None,
+    ):
+        self.uuid: UUID4
+        self.name = name
+        self.description = description
+        self.stakeholder = stakeholder
+        self.category = category
+        self.parameter = parameter
+
+    def __repr__(self):
+        return (
+            "<Expectation: name=%s, description=%s, stakeholder=%s, category=%s, uuid=%s>"
+            % (self.name, self.description, self.stakeholder, self.category, self.uuid)
+        )
+
+
+class Need(BaseModel):
+    def __init__(self, name, description=None, stakeholder=None, category=None):
+        self.uuid: UUID4
         self.name = name
         self.description = description
         self.stakeholder = stakeholder
         self.category = category
 
     def __repr__(self):
-        return "<Expectation: name=%s, description=%s, stakeholder=%s, category=%s, uuid=%s>" % (self.name, self.description, self.stakeholder, self.category, self.uuid)
+        return (
+            "<Need: name=%s, description=%s, stakeholder=%s, category=%s, uuid=%s>"
+            % (self.name, self.description, self.stakeholder, self.category, self.uuid)
+        )
 
 
-class Need:
-    def __init__(self,
-                 name,
-                 description=None,
-                 stakeholder=None,
-                 category=None):
-        self.uuid = uuid.uuid4()
-        self.name = name
-        self.description = description
-        self.stakeholder = stakeholder
-        self.category = category
-
-    def __repr__(self):
-        return "<Need: name=%s, description=%s, stakeholder=%s, category=%s, uuid=%s>" % (self.name, self.description, self.stakeholder, self.category, self.uuid)
-
-
-class NeedCategory:
-    def __init__(self,
-                 name,
-                 description=None):
-        self.uuid = uuid.uuid4()
+class NeedCategory(BaseModel):
+    def __init__(self, name, description=None):
+        self.uuid: UUID4
         self.name = name
         self.description = description
 
@@ -68,11 +101,9 @@ class NeedCategory:
         return "<NeedCategory= name=%s, description=%s>" % (self.name, self.description)
 
 
-class ValueDriver:
-    def __init__(self,
-                 name,
-                 description=None):
-        self.uuid = uuid.uuid4()
+class ValueDriver(BaseModel):
+    def __init__(self, name, description=None):
+        self.uuid: UUID4
         self.name = name
         self.description = description
 
@@ -80,35 +111,35 @@ class ValueDriver:
         return "<ValueDriver: name=%s, description=%s>" % (self.name, self.description)
 
 
-class DesignSolution:
-    def __init__(self,
-                 name,
-                 description=None):
-        self.uuid = uuid.uuid4()
+class DesignSolution(BaseModel):
+    def __init__(self, name, description=None):
+        self.uuid: UUID4
         self.name = name
         self.description = description
 
     def __repr__(self):
-        return "<DesignSolution: name=%s, description=%s>" % (self.name, self.description)
+        return "<DesignSolution: name=%s, description=%s>" % (
+            self.name,
+            self.description,
+        )
 
 
-class FunctionalRequirement:
-    def __init__(self,
-                 name,
-                 description=None):
-        self.uuid = uuid.uuid4()
+class FunctionalRequirement(BaseModel):
+    def __init__(self, name, description=None):
+        self.uuid: UUID4
         self.name = name
         self.description = description
 
     def __repr__(self):
-        return "<FunctionalRequirement: name=%s, description=%s>" % (self.name, self.description)
+        return "<FunctionalRequirement: name=%s, description=%s>" % (
+            self.name,
+            self.description,
+        )
 
 
-class Constraint:
-    def __init__(self,
-                 name,
-                 description=None):
-        self.uuid = uuid.uuid4()
+class Constraint(BaseModel):
+    def __init__(self, name, description=None):
+        self.uuid: UUID4
         self.name = name
         self.description = description
 
@@ -116,69 +147,50 @@ class Constraint:
         return "<Constraint: name=%s, description=%s>" % (self.name, self.description)
 
 
-class ProductProject:
-    def __init__(self,
-                 name,
-                 description=None):
-        self.uuid = uuid.uuid4()
+class ProductProject(BaseModel):
+    def __init__(self, name, description=None):
+        self.uuid: UUID4
         self.name = name
         self.description = description
 
     def __repr__(self):
-        return "<ProductProject: name=%s, description=%s>" % (self.name, self.description)
+        return "<ProductProject: name=%s, description=%s>" % (
+            self.name,
+            self.description,
+        )
 
 
-class ConfigurableComponent:
-    def __init__(self,
-                 name,
-                 description=None):
-        self.uuid = uuid.uuid4()
+class ConfigurableComponent(BaseModel):
+    def __init__(self, name, description=None):
+        self.uuid: UUID4
         self.name = name
         self.description = description
 
     def __repr__(self):
-        return "<ConfigurableComponent: name=%s, description=%s>" % (self.name, self.description)
+        return "<ConfigurableComponent: name=%s, description=%s>" % (
+            self.name,
+            self.description,
+        )
 
 
-class Parameter:
-    def __init__(self,
-                 name,
-                 description=None,
-                 parameter_type=None,
-                 min=None,
-                 max=None,
-                 value=None):
-        self.uuid = uuid.uuid4()
-        self.name = name
-        self.description = description
-        self.parameter_type = parameter_type,
-        self.min = min,
-        self.max = max,
-        self.value = value
-
-    def __repr__(self):
-        return "<Parameter: name=%s, description=%s>" % (self.name, self.description)
-
-
-class Interaction:
-    def __init__(self,
-                 category,
-                 origin,
-                 target):
-        self.uuid = uuid.uuid4()
+class Interaction(BaseModel):
+    def __init__(self, category, origin, target):
+        self.uuid: UUID4
         self.category = category
         self.origin = origin
         self.target = target
 
     def __repr__(self):
-        return "<Interaction: category=%s, origin=%s, target=%s>" % (self.category, self.origin, self.target)
+        return "<Interaction: category=%s, origin=%s, target=%s>" % (
+            self.category,
+            self.origin,
+            self.target,
+        )
 
 
-class Platform:
-    def __init__(self,
-                 name,
-                 description=None):
-        self.uuid = uuid.uuid4()
+class Platform(BaseModel):
+    def __init__(self, name: str, description: Optional[str] = None):
+        self.uuid: UUID4
         self.name = name
         self.description = description
 
@@ -186,8 +198,26 @@ class Platform:
         return "<Platform: name=%s, description=%s>" % (self.name, self.description)
 
 
-def main():
-    pass
+class Product(BaseModel):
+    def __init__(self, name: str, description: Optional[str] = None):
+        self.uuid: UUID4
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return "<Product: name=%s, description=%s>" % (self.name, self.description)
+
+def main() -> None:
+    """Main function."""
+
+    # Read data from a JSON file
+    with open("./data.json") as file:
+        data = json.load(file)
+        books: List[Book] = [Book(**item) for item in data]
+        # print(books)
+        print(books[0])
+        # print(books[0].dict(exclude={"price"}))
+        # print(books[1].copy())
 
 
 if __name__ == "__main__":
